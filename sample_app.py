0175b36ffc9a1546e98c54f9c3156566da36ac25
tanushsunil <107385446+tanushsunil@users.noreply.github.com>
2022-06-18 00:29:45 +0530

    Update sample_app.py

--- sample_app.py
+++ sample_app.py
@@ -7,5 +7,9 @@ Hello world!
 """)
 
 dataset = pd.read_csv('./patient_data.csv')
-st.title("Hello world!")  # add a title
-st.write(dataset)
+st.title("Hello world!")
+
+uploaded_file = st.file_uploader("Upload a harshith")
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.write(dataframe)
