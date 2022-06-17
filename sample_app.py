71fe46e00d96ad1a87a2ac73508e2a203cf0d406
tanushsunil <107385446+tanushsunil@users.noreply.github.com>
2022-06-18 00:44:32 +0530

    Update sample_app.py

--- sample_app.py
+++ sample_app.py
@@ -13,17 +13,3 @@ uploaded_file = st.file_uploader("Upload a harshith")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   st.write(df)
-  
-  fig, ax = plt.subplots()
-  df.hist(
-    bins=8,
-    column="year",
-    row="danceability",
-    grid=False,
-    figsize=(8, 8),
-    color="#86bf90",
-    zorder=2,
-    rwidth=0.9,
-    ax=ax,
-  )
-  st.write(fig)
