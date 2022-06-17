bfcf075172273a1f6edb89e0cb7a78e786fdf26a
tanushsunil <107385446+tanushsunil@users.noreply.github.com>
2022-06-18 00:34:32 +0530

    Update sample_app.py

--- sample_app.py
+++ sample_app.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
 
@@ -12,3 +13,17 @@ uploaded_file = st.file_uploader("Upload a harshith")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   st.write(df)
+  
+  fig, ax = plt.subplots()
+  df.hist(
+    bins=8,
+    column="year",
+    row="danceability",
+    grid=False,
+    figsize=(8, 8),
+    color="#86bf91",
+    zorder=2,
+    rwidth=0.9,
+    ax=ax,
+  )
+  st.write(fig)
