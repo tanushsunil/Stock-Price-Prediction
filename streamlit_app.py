4dd79f59d383df494a394eae1538be69426c330d
tanushsunil <107385446+tanushsunil@users.noreply.github.com>
2022-06-23 01:25:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model = pickle.load(open(r'Models\linear_regression.sav', 'rb'))
+loaded_model = pickle.load(open('Models\linear_regression.sav', 'rb'))
 
 
 # creating a function for Prediction
